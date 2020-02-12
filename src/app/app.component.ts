fd5ab8ea2037af95c71c4e41c517e66afc045bbe
Aruna <anura4510@gmail.com>
2020-02-12 11:27:38 +0000

    new commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = {
+    name:"aruna",
+    lastname:"elango"
+  };
+
+  arr=[3,4,5];
+
+  namearr=['aruna','uma','gowrri']
+}
